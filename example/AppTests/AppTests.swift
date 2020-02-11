f82a55f262ec3321251382eba581fda59e8393d2
Ben <b.cylin@gmail.com>
2020-02-11 22:11:53 +0000

    Update tests to include Swift Packages

--- example/AppTests/AppTests.swift
+++ example/AppTests/AppTests.swift
@@ -10,6 +10,16 @@ import XCTest
 
 final class AppTests: XCTestCase {
 
+  private let expectedList = [
+    "Alamofire",
+    "CPDAcknowledgements",
+    "Crypto",
+    "CwlCatchException",
+    "CwlPreconditionTesting",
+    "Nimble",
+    "Strongify"
+  ]
+
   func testAcknowledgementsPlist() {
     let resourcePath = URL(string: Bundle.main.resourcePath!)!
     let plistPath = resourcePath.appendingPathComponent("Pods-App-metadata.plist")
@@ -17,7 +27,7 @@ final class AppTests: XCTestCase {
     let dictionary = NSDictionary(contentsOfFile: plistPath.absoluteString)!
     let acknowledgements = (dictionary["specs"] as! [[String: Any]]).map { $0["name"] } as? [String]
 
-    XCTAssertEqual(acknowledgements, ["Alamofire", "CPDAcknowledgements", "Crypto", "Strongify"])
+    XCTAssertEqual(acknowledgements, expectedList)
   }
 
   func testSettingsPlist() {
@@ -29,7 +39,7 @@ final class AppTests: XCTestCase {
     let acknowledgements = (dictionary["PreferenceSpecifiers"] as! [[String: Any]]).map { $0["Title"] } as? [String]
     let footer = [""]
 
-    XCTAssertEqual(acknowledgements, header + ["Alamofire", "CPDAcknowledgements", "Crypto", "Strongify"] + footer)
+    XCTAssertEqual(acknowledgements, header + expectedList + footer)
   }
 
 }
